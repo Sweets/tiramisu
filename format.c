44e906556235880f8a2e4183c5ddcdbd4476fe85
Sweets <Sweets@users.noreply.github.com>
2020-07-01 10:17:21 -0700

    forgot a comma boys

--- format.c
+++ format.c
@@ -26,7 +26,7 @@ void output_notification(gchar *app_name, guint32 replaces_id, gchar *app_icon,
     
     printf("{ \"app_name\": \"%s\",", escape_quotes(app_name, escaped_string));
     printf(" \"app_icon\": \"%s\",", escape_quotes(app_icon, escaped_string));
-    printf(" \"replaces_id\": %u, \"timeout\": %d," replaces_id, timeout);
+    printf(" \"replaces_id\": %u, \"timeout\": %d,", replaces_id, timeout);
     printf(" \"summary\": \"%s\",", escape_quotes(summary, escaped_string));
     printf(" \"body\": \"%s\" }\n", escape_quotes(body, escaped_string));
     
