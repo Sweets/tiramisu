f161a918fa18cc3acfe513eda85094362ea59cc4
Sweets <sweets@comfi.es>
2021-10-11 21:14:03 -0700

    Remove C source files

--- src/events.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <gio/gio.h>
-#include <glib.h>
-
-void bus_acquired(GDBusConnection*, const gchar *, gpointer);
-void name_acquired(GDBusConnection*, const gchar *, gpointer);
-void name_lost(GDBusConnection*, const gchar *, gpointer);
